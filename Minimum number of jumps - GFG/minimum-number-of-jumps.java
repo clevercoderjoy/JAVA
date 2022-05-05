95a0c9080584a93677dcd75bcb7cfc9f3dc92311
clevercoderjoy <74505057+clevercoderjoy@users.noreply.github.com>
2022-05-05 15:43:35 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.java
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.java
@@ -28,6 +28,32 @@ class GFG
 class Solution{
     static int minJumps(int[] arr){
         // your code here
+        // int n = arr.length;
+        // if(n <= 1){
+        //     return 0;
+        // }
+        // if(arr[0] == 0){
+        //     return -1;
+        // }
+        // int maxReach = arr[0];
+        // int step = arr[0];
+        // int jump = 1;
+        // for(int i = 1; i < n; i++){
+        //     if(i == n - 1){
+        //         return jump;
+        //     }
+        //     maxReach = Math.max(maxReach, arr[i] + i);
+        //     step--;
+        //     if(step == 0){
+        //         jump++;
+        //         if(i >= maxReach){
+        //             return -1;
+        //         }
+        //         step = maxReach - i;
+        //     }
+        // }
+        // return jump;
+        
         int n = arr.length;
         if(n <= 1){
             return 0;
@@ -36,20 +62,20 @@ class Solution{
             return -1;
         }
         int maxReach = arr[0];
-        int step = arr[0];
+        int steps = arr[0];
         int jump = 1;
         for(int i = 1; i < n; i++){
             if(i == n - 1){
                 return jump;
             }
             maxReach = Math.max(maxReach, arr[i] + i);
-            step--;
-            if(step == 0){
+            steps--;
+            if(steps == 0){
                 jump++;
+                steps = maxReach - i;
                 if(i >= maxReach){
                     return -1;
                 }
-                step = maxReach - i;
             }
         }
         return jump;
