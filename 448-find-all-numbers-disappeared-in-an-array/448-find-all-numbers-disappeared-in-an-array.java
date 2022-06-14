f9395632620bd8bf3882d93ff343b8836b43ff12
clevercoderjoy <74505057+clevercoderjoy@users.noreply.github.com>
2022-06-14 14:07:28 +0530

    Time: 10 ms (43.94%), Space: 66.2 MB (55.16%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
         int i = 0;
-        int n = nums.length;
-            List<Integer> ans = new ArrayList<>();
-        while(i < n){
+        while(i < nums.length){
             int correctIndex = nums[i] - 1;
-            if(nums[i] != nums[correctIndex]){
+            if(nums[i] !=  nums[correctIndex]){
                 swap(nums, i, correctIndex);
             }
-            else
+            else{
                 i++;
+            }
         }
-        for(int j = 0; j < n; j++){
-            if(nums[j] != j + 1)
-                ans.add(j + 1);
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != j + 1){
+                res.add(j + 1);
+            }
         }
-        return ans;
+        return res;
     }
     
-    public void swap(int[] nums, int first, int second){
-        int temp = nums[first];
-        nums[first] = nums[second];
-        nums[second] = temp;
+    public void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
