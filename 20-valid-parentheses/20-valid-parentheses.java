d71adec04aa5d72344e91db577afff244e10a57e
clevercoderjoy <74505057+clevercoderjoy@users.noreply.github.com>
2022-07-25 13:12:54 +0530

    Time: 2 ms (90.26%), Space: 42.5 MB (17.02%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() % 2 != 0){
+        if(s.length() % 2 != 0)
             return false;
-        }
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack();
         for(char ch : s.toCharArray()){
-            if(ch == '(' || ch == '{' || ch == '['){
+            if(ch == '(' || ch == '{' || ch == '[')
                 stack.push(ch);
-            }
-            else if(!stack.isEmpty() && ch == ')' && stack.peek() == '('){
+            else if(!stack.isEmpty() && stack.peek() == '(' && ch == ')')
                 stack.pop();
-            }
-            else if(!stack.isEmpty() && ch == '}' && stack.peek() == '{'){
+            else if(!stack.isEmpty() && stack.peek() == '{' && ch == '}')
                 stack.pop();
-            }
-            else if(!stack.isEmpty() && ch == ']' && stack.peek() == '['){
+            else if(!stack.isEmpty() && stack.peek() == '[' && ch == ']')
                 stack.pop();
-            }
-            else{
+            else
                 return false;
-            }
         }
         return stack.isEmpty();
     }
