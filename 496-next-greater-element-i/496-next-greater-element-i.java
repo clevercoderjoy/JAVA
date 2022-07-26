83a3e97adaf42ba8c6bae23d5f65013ad30da820
clevercoderjoy <74505057+clevercoderjoy@users.noreply.github.com>
2022-07-26 12:02:17 +0530

    Time: 8 ms (24.67%), Space: 43.6 MB (77.46%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,32 +1,15 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int ngr[] = nextGreaterRight(nums2);
-        for(int i = 0; i < nums2.length; i++){
-            map.put(nums2[i], ngr[i]);
+        HashMap<Integer, Integer> nextG = new HashMap<>();
+        Stack<Integer> stack = new Stack();
+        for(Integer num : nums2){
+            while(!stack.isEmpty() && stack.peek() < num)
+                nextG.put(stack.pop(), num);
+            stack.push(num);
         }
-        int[] ans = new int[nums1.length];
         for(int i = 0; i < nums1.length; i++){
-            ans[i] = map.get(nums1[i]);
+            nums1[i] = nextG.getOrDefault(nums1[i], -1);
         }
-        return ans;
-    }
-    
-    public int[] nextGreaterRight(int[] nums){
-        Stack<Integer> st = new Stack<>();
-        int[] ans = new int[nums.length];
-        for(int i = nums.length - 1; i >= 0; i--){
-            while(st.size() > 0 && st.peek() < nums[i]){
-                st.pop();
-            }
-            if(st.size() > 0){
-                ans[i] = st.peek();
-            }
-            else{
-                ans[i] = -1;
-            }
-            st.push(nums[i]);
-        }
-        return ans;
+        return nums1;
     }
 }
